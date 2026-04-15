48d90b41d5bfa07ccf9abb30eeddb4c03930a09d
Santiago Ospina <santiagoospina@Santiagos-MacBook-Air.local>
2026-04-14 20:34:19 -0500

    fix: no-cache headers for /quoter route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/quoter",
+        headers: [
+          { key: "Cache-Control", value: "no-store, must-revalidate" },
+          { key: "CDN-Cache-Control", value: "no-store" },
+          { key: "Vercel-CDN-Cache-Control", value: "no-store" },
+        ],
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
